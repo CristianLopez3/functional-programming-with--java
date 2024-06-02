ca0d520768abb2e5616cab65206c72f057721fcc
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-06-02 12:39:41 -0500

    functional interfaces and method references

--- src/main/java/com/finance/programming/FP03FunctionalInterfaces2.java
+++ src/main/java/com/finance/programming/FP03FunctionalInterfaces2.java
@@ -1,27 +1,99 @@
 package com.finance.programming;
 
 import java.util.List;
+import java.util.Random;
 import java.util.function.*;
 import java.util.stream.Collectors;
 
 public class FP03FunctionalInterfaces2 {
 
-	
+    /**
+     * IntBinaryOperator
+     * IntConsumer
+     * IntFunction
+     * IntPredicate
+     * IntSupplier
+     * IntToDoubleFunction
+     * IntToLongFunction
+     */
+
+    private static List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+
 	public static void main(String[] args) {
+//        predicate();
+//        consumer();
+//        function();
+//        binaryOperator();
+//        unaryOperator(3);
+//        biPredicate();
+//	      biFunction();
+//        biConsumer();
+    }
 
-		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-		Predicate<Integer> evenPredicate = x -> x % 2 == 0;
-		Predicate<Integer> oddPredicate = x -> x % 2 != 0;
 
-//		filterAndPrint(numbers, evenPredicate);
-		filterAndPrint(numbers, (x) -> x % 3 == 0);
+    private static void biConsumer(){
+        BiConsumer<String, String> biConsumer = (str1, str2) -> {
+            System.out.println(str1);
+            System.out.println(str2);
+        };
+        biConsumer.accept("Hello", "World!");
+    }
 
 
-		Function<Integer, Integer> mapper = x -> x * x;
+    private static void biFunction(){
+        BiFunction<Integer, Integer, String> biFunction = (x, y) -> String.format("""
+            number 1: %d
+            number 2: %d
+        """, x, y);
+        System.out.println(biFunction.apply(2, 3));
+    }
 
-		mapAndCreateList(numbers, mapper);
 
-	}
+    private static void biPredicate(){
+        BiPredicate<Integer, String> biPredicate = (x, y) -> x <= y.length();
+        System.out.println(biPredicate.test(5, "Hello World"));
+    }
+
+
+    private static void predicate(){
+        Predicate<Integer> evenPredicate = x -> x % 2 == 0;
+        Predicate<Integer> oddPredicate = x -> x % 2 != 0;
+
+        filterAndPrint(numbers, evenPredicate);
+        filterAndPrint(numbers, (x) -> x % 3 == 0);
+    }
+
+
+    private static void consumer(){
+        Consumer<Integer> sysoutInteger = System.out::println;
+    }
+
+    
+    private static void function(){
+        Function<Integer, Integer> mapper = x -> x * x;
+        Function<Integer, String> stringMapper = x -> x + "";
+
+        mapAndCreateList(numbers, mapper);
+    }
+
+
+    private static void binaryOperator(){
+        BinaryOperator<Integer> sumBinaryOperator = Integer::sum;
+
+        Supplier<Integer> randomIntegerSupplier = () -> {
+            Random random = new Random();
+            return random.nextInt(1000);
+        };
+
+        System.out.println(randomIntegerSupplier.get());
+    }
+
+
+    private static void unaryOperator(int number){
+        UnaryOperator<Integer> unaryOperator = x -> number * x;
+        System.out.println(unaryOperator.apply(3));
+    }
+
 
 	private static void mapAndCreateList(List<Integer> numbers, Function<Integer, Integer> mapper) {
 		numbers.stream()
