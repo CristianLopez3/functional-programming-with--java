223c925d80fbc51e4a7687923bbfed5fbf2188bc
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-06-02 17:15:57 -0500

    use IntStreams and Long Streams and perform operations with BigInteger

---
+++ src/main/java/com/finance/programming/FP06StreamOf.java
@@ -0,0 +1,51 @@
+package com.finance.programming;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class FP06StreamOf {
+
+
+
+    public static void main(String[] args) {
+        System.out.println(IntStream.range(1, 10).sum());
+
+        System.out.println(IntStream.iterate(1, e -> e + 2).limit(10));
+
+        System.out.println(IntStream
+                .iterate(1, e -> e + 2)
+                .limit(10)
+                .peek(System.out::println)
+                .sum()
+        );
+
+        IntStream
+                .iterate(2, e -> e + 2)
+                .limit(10)
+                .peek(System.out::println)
+                .sum();
+
+        IntStream
+                .iterate(2, e -> e * 2)
+                .limit(10)
+                .peek(System.out::println)
+                .sum();
+
+        System.out.println(IntStream
+                .iterate(2, e -> e + 2)
+                .limit(10)
+                .boxed()
+                .toList());
+
+
+        System.out.println(LongStream
+                .rangeClosed(1, 10)
+                .mapToObj(BigDecimal::valueOf)
+                .reduce(BigDecimal.ONE, BigDecimal::multiply));
+
+
+    }
+
+}
