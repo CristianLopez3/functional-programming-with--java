1332e352de4adbce2f95a1ab3f9b0e914ac5a47b
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-06-02 19:52:27 -0500

    working with files

--- src/main/java/com/finance/programming/FP05Files.java
+++ src/main/java/com/finance/programming/FP05Files.java
@@ -3,21 +3,23 @@ package com.finance.programming;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class FP05Files {
 
 	public static void main(String[] args) throws IOException {
-		
-		// Files.lines(Paths.get("file.txt"))
-		// .map(str -> str.split(" "))
-		// .flatMap(Arrays::stream)
-		// .distinct()
-		// .sorted()
-		// .forEach(System.out::println);
-		
+
+		Files.lines(Paths.get("text.txt"))
+				.map(str -> str.split(" "))
+				.flatMap(Arrays::stream)
+				.distinct()
+				.sorted()
+				.forEach(System.out::println);
+
 		Files.list(Paths.get("."))
-			.filter(Files::isDirectory)
-			.forEach(System.out::println);
+				.filter(Files::isDirectory)
+				.forEach(System.out::println);
 
 	}
 
