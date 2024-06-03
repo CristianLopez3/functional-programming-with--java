09804c0d0f0c26768dd5f60f1a5cfe02920d3c2c
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2024-06-02 19:07:46 -0500

    parallelism with functional programming

--- src/main/java/com/finance/programming/FP05Parallelizing.java
+++ src/main/java/com/finance/programming/FP05Parallelizing.java
@@ -5,15 +5,26 @@ import java.util.stream.LongStream;
 public class FP05Parallelizing {
 	
 	public static void main(String[] args) {
-		long time = System.currentTimeMillis();
+		System.out.println("WITHOUT PARALLELISM");
+		withoutParallelism();
+		// WITH PARALLELISM
 
-		//0, 1000000000 869
-		//System.out.println(LongStream.range(0,1000000000).sum());//499999999500000000
-		
-		//601
-		System.out.println(LongStream.range(0,1000000000).parallel().sum());
-		
+		System.out.println("WITH PARALLELISM");
+		long time = System.currentTimeMillis();
+		// 0, 1000000
+		System.out.println(LongStream.range(0, 100000).parallel().sum());
 		System.out.println(System.currentTimeMillis() - time);
+
 	}
+
+	private static void withoutParallelism(){
+
+		long time = System.currentTimeMillis();
+		// 0, 1000000
+		System.out.println(LongStream.range(0, 100000).sum());
+		System.out.println(System.currentTimeMillis() - time);
+
+	}
+
 
 }
